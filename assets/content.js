0360aec9218660f4e2909830e6266ef85c0fccc6
飯田 意己 <lohocla@icloud.com>
2017-01-28 22:50:09 +0900

    markdown-ized

---
+++ assets/content.js
@@ -0,0 +1,77 @@
+var content = (function () {/*
+# @yo-iida
+
+Software Engineer
+
+_Alcohol Driven Development_
+
+# Experience
+
+## Software Engineer
+
+### CrowdWorks
+
+New Business Feature Development,
+
+Team Building, Scrum Master, ...
+
+2015/5 - now
+
+## Engineer / Web Director
+
+### iact
+
+Planning, Design, Development,
+
+Operation and maintenance, ...
+
+2012/4 - 2015/3
+
+# Skills
+
+Ruby on Rails, Agile, Scrum, Project Management,
+
+JavaScript(ES2015), React, Redux, Webpack,
+
+Rspec, Capybara, CI, PostgreSQL, Redis, MySQL,
+
+PHP, Git, Linux, AWS, Heroku, Slack
+
+# Writing
+
+## CrowdWorks Engineer Blog
+
+* [Herokuで本番サービスを運用する際にやっておきたいこと & 構成の事例](http://engineer.crowdworks.jp/entry/2016/09/05/154304)
+* [エンジニア×他部署のチームビルディングにおいて重要なこと](http://engineer.crowdworks.jp/entry/2016/04/12/103010)
+
+## Qiita
+
+* [Qiita](http://qiita.com/yo-iida)
+
+# Education
+
+## Osaka University
+
+Bachelor of Engineering
+
+Department of Electronics and Material Physics, School of Engineering Science
+
+2008/4 - 2012/3
+
+## Kanazawa Sakuragaoka High School
+
+2005/4 - 2008/3
+
+# Interests
+
+Photos, Arts, Cooking, Drinking, Swimming
+
+# Get in touch
+
+* [github](http://github.com/yo-iida)
+* [facebook](https://www.facebook.com/yoshiki.iida1)
+* [twitter](https://twitter.com/ysk_118)
+* [hatena blog](http://yo-iida.hatenablog.com/)
+* [instagram](https://www.instagram.com/yoshikiiida)
+
+*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
